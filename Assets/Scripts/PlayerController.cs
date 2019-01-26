f6c8afa925ea978122a7549e2096a101c54f8eb3
Matthew Caldwell <caldwellysr@gmail.com>
2019-01-26 00:17:11 -0500

    Add non-physics player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+  [Range(0.05f, 0.5f)]
+  public float playerSpeed;
+
+  [Range(1f, 5f)]
+  public float turnSpeed;
+
+  void FixedUpdate()
+  {
+    var throttle = Input.GetAxis("Vertical");
+    transform.Translate(0, playerSpeed * throttle, 0);
+
+    var turnAmount = Input.GetAxis("Horizontal");
+    transform.Rotate(0, 0, turnSpeed * -turnAmount);
+  }
+}
