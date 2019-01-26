719bc0caf0ba61c0c4def0681637c9b19f42f092
Quin Hagerty <hagertyq@gmail.com>
2019-01-26 15:54:27 -0500

    + brush & + brush rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,12 +11,24 @@ public class PlayerController : MonoBehaviour
 
   public string throttleInput, turnInput;
 
-  void FixedUpdate()
+    public GameObject Brush; 
+
+    void FixedUpdate()
   {
     var throttle = Input.GetAxis(throttleInput);
     transform.Translate(0, playerSpeed * throttle, 0);
 
     var turn = (throttle <= -0.05f) ? -1f : 1f;
     transform.Rotate(0, 0, turn * turnSpeed * -Input.GetAxis(turnInput));
-  }
+
+        if (gameObject.activeSelf)
+        {
+            RotateBrush();
+        }
+    }
+
+    public void RotateBrush()
+    {
+        Brush.transform.Rotate(0, 0, 30);
+    }
 }
